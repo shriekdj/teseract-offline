4f989ff48060a63d473b87db75459a0646954c0d
Shrikant Dhayje <shrikantdhayaje@gmail.com>
2022-08-02 15:10:04 +0530

    Modifications

--- node/index.js
+++ node/index.js
@@ -1,5 +1,6 @@
 const { createWorker } = require('tesseract.js');
 const path = require('path');
+const { fs } = require('fs');
 
 const worker = createWorker({
   langPath: path.join(__dirname, '..', 'lang-data'), 
@@ -10,7 +11,14 @@ const worker = createWorker({
   await worker.load();
   await worker.loadLanguage('eng');
   await worker.initialize('eng');
-  const { data: { text } } = await worker.recognize(path.join(__dirname, '..', 'images', 'testocr.png'));
+  const { data: { text } } = await worker.recognize(path.join(__dirname, '..', 'images', 'all-certificates-001.jpg'));
   console.log(text);
+	fs.writeFile('result.txt', text, err => {
+		if (err) {
+			console.error(err);
+		}
+		// file written successfully
+	});
   await worker.terminate();
 })();
+
